3af62b1e134dd358d3b584391590b7455de0e194
prusova_y <y-prusova@gmail.com>
2021-10-19 18:34:57 +0300

    Something has been done

--- subsidiary/form_report.py
+++ subsidiary/form_report.py
@@ -1,5 +1,6 @@
 from .log_record import LogRecord, RequestType
 from collections import ItemsView
+from json import dumps
 from ipaddress import IPv4Address
 from time import strftime
 from typing import Tuple, List, Dict
@@ -9,11 +10,22 @@ LOGS_TYPE = Tuple[LogRecord]
 
 
 def count_record_by_request_type(logs: LOGS_TYPE, required_type: int) -> int:
+    """
+    Count amount of requests of the type
+    :param logs: list of logs
+    :param required_type: type of request
+    :return: amount of requests
+    """
     suitable_logs: List[LogRecord] = [log for log in logs if log.request_type == required_type]
     return len(suitable_logs)
 
 
 def summarize_record_by_client(logs: LOGS_TYPE) -> Dict[IPv4Address, int]:
+    """
+    Count amount of requests by every client
+    :param logs: list of logs
+    :return: clients IP: amount of requests
+    """
     amounts: Dict[IPv4Address, int] = dict()
     for log in logs:
         client = log.client_ip
@@ -22,34 +34,52 @@ def summarize_record_by_client(logs: LOGS_TYPE) -> Dict[IPv4Address, int]:
     return amounts
 
 
-def get_top_clients(logs: LOGS_TYPE) -> str:
+def get_top_clients(logs: LOGS_TYPE, amount: int) -> ItemsView[IPv4Address, int]:
+    """
+    Count amount of request by every client
+    :param logs: list of logs
+    :param amount: required amount
+    :return: required amount of clients with the highest number of requests
+    """
     amount_logs_by_client: Dict[IPv4Address, int] = summarize_record_by_client(logs)
     sorted_logs_by_client: ItemsView[IPv4Address, int] = sorted(amount_logs_by_client.items(),
                                                                 key=lambda _item: _item[1], reverse=True)
-    top_clients_list: List[str] = list()
-    for item in sorted_logs_by_client[:3]:
-        top_clients_list.append(f"\t{str(item[0])}: {item[1]} requests")
-    return str('\n').join(top_clients_list)
+    return sorted_logs_by_client[:amount]
 
 
-def get_top_requests(logs: LOGS_TYPE) -> str:
+def get_top_requests(logs: LOGS_TYPE, amount: int) -> List[LogRecord]:
+    """
+    Sorted list of logs by duration of request
+    :param logs: list of logs
+    :param amount: required amount of logs
+    :return: required amount of the longest requests
+    """
     sorted_logs_by_duration = sorted(logs, key=lambda log: log.duration, reverse=True)
-    top_request_list: List[str] = list()
-    for log in sorted_logs_by_duration[:3]:
-        top_request_list.append(f"\tTYPE: {log.request_type_name} URL: {log.url} IP: {log.client_ip} "
-                                f"TIME: {strftime('%d-%m-%Y %H:%M:%S')} DURATION: {log.request_duration}")
-    return str('\n').join(top_request_list)
+    return sorted_logs_by_duration[:amount]
 
 
-def form(logs: LOGS_TYPE) -> str:
+def form(logs: LOGS_TYPE) -> (str, str):
+    """
+    Form report in string and json formats
+    :param logs: list of logs
+    :return: string format, json format
+    """
+
     all_logs_amount = len(logs)
     get_logs_amount = count_record_by_request_type(logs, RequestType.GET)
     post_logs_amount = count_record_by_request_type(logs, RequestType.POST)
     put_logs_amount = count_record_by_request_type(logs, RequestType.PUT)
     delete_logs_amount = count_record_by_request_type(logs, RequestType.DELETE)
 
-    top_clients = get_top_clients(logs)
-    top_requests = get_top_requests(logs)
+    top_clients: ItemsView[IPv4Address, int] = get_top_clients(logs, 3)
+    top_clients_str = [f"\t{str(item[0])}: {item[1]} requests" for item in top_clients]
+    top_clients_str = str('\n').join(top_clients_str)
+
+    top_requests: List[LogRecord] = get_top_requests(logs, 3)
+    top_requests_str = [f"\tTYPE: {log.request_type_name} URL: {log.url} IP: {log.client_ip} " \
+                        f"TIME: {strftime('%d-%m-%Y %H:%M:%S', log.date)} DURATION: {log.request_duration}"
+                        for log in top_requests]
+    top_requests_str = str('\n').join(top_requests_str)
 
     report_str = f"Logs amount: {all_logs_amount}\n" \
                  f"Logs amount by requests type:\n" \
@@ -59,10 +89,10 @@ def form(logs: LOGS_TYPE) -> str:
                  f"\tDELETE: {delete_logs_amount}\n" \
                  f"\n" \
                  f"TOP3 clients:\n" \
-                 f"{top_clients}\n" \
+                 f"{top_clients_str}\n" \
                  f"\n" \
                  f"TOP3 requests:\n" \
-                 f"{top_requests}"
+                 f"{top_requests_str}"
 
     report_dict = {
         "logs_amount": {
@@ -72,10 +102,14 @@ def form(logs: LOGS_TYPE) -> str:
             "PUT": put_logs_amount,
             "DELETE": delete_logs_amount
         },
-        "top3_client":
+        "top3_clients": [(str(ip), requests_amount) for ip, requests_amount in top_clients],
+        "top3_requests": [{
+            "TYPE": log.request_type_name,
+            "URL": log.url,
+            "IP": log.client_ip,
+            "TIME": strftime('%d-%m-%Y %H:%M:%S', log.date),
+            "DURATION": log.request_duration
+        } for log in top_requests]
     }
 
-
-
-
-
+    return report_str, dumps(report_dict, indent=3)
